2d745ef793c4497efebb3140750c3404b3446087
jonbarnold <jon-arnold@outlook.com>
2026-03-28 14:46:51 +0000

    Deploying to gh-pages from @ jonbarnold/jonbarnold.github.io@64c656c4c53b3e8b3fe9804223677c566b3e9111 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jonbarnold/al-folio",
+    "data-repo": "jonbarnold/jonbarnold.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
